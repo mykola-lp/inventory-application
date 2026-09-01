f0c582287c01042a50bc02c9ae901c4bb458b17d
mykola-lp <mykola.sukhorab@gmail.com>
2026-09-01 09:23:51 +0300

    implement indexGet func

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,15 @@
 // controllers/indexController.js
+const db = require("../db/queries");
 
 async function indexGet(req, res) {
-  res.send("Index page — list of categories");
+  const categories = await db.getAllCategories();
+
+  res.render(
+    "index", {
+      title: "Horse Store",
+      categories: categories
+    }
+  );
 }
 
 module.exports = { indexGet };
