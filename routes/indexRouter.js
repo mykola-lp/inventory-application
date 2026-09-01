7955972e7b7db380d324c6cf1c0cfba08f527259
mykola-lp <mykola.sukhorab@gmail.com>
2026-09-01 08:59:50 +0300

    add index Router

---
+++ routes/indexRouter.js
@@ -0,0 +1,11 @@
+// routes/indexRouter.js
+
+const { Router } = require("express");
+
+const indexController = require("../controllers/indexController");
+
+const indexRouter = Router();
+
+indexRouter.get("/", indexController.indexGet);
+
+module.exports = indexRouter;
