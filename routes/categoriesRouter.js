cf1cae244408878ccb6236a5435b685e0a0bd661
mykola-lp <mykola.sukhorab@gmail.com>
2026-09-01 11:59:58 +0300

    wire up delete routes for categories and items

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,7 +1,6 @@
 // routes/categoriesRouter.js
 
 const { Router } = require("express");
-
 const categoriesController = require("../controllers/categoriesController");
 
 const categoriesRouter = Router();
@@ -11,6 +10,7 @@ categoriesRouter.post("/new", categoriesController.categoryCreatePost);
 categoriesRouter.get("/:id", categoriesController.categoryDetailGet);
 categoriesRouter.get("/:id/update", categoriesController.categoryUpdateGet);
 categoriesRouter.post("/:id/update", categoriesController.categoryUpdatePost);
+categoriesRouter.get("/:id/delete", categoriesController.categoryDeleteGet);
 categoriesRouter.post("/:id/delete", categoriesController.categoryDeletePost);
 
 module.exports = categoriesRouter;
--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -11,6 +11,7 @@ itemsRouter.post("/new", itemsController.itemCreatePost);
 itemsRouter.get("/:id", itemsController.itemDetailGet);
 itemsRouter.get("/:id/update", itemsController.itemUpdateGet);
 itemsRouter.post("/:id/update", itemsController.itemUpdatePost);
+itemsRouter.get("/:id/delete", itemsController.itemDeleteGet);
 itemsRouter.post("/:id/delete", itemsController.itemDeletePost);
 
 module.exports = itemsRouter;
