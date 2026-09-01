a06de0fb83eefee2ec1ddf18bfee8e71e75bbbe4
mykola-lp <mykola.sukhorab@gmail.com>
2026-09-01 10:05:19 +0300

    write queries

--- db/queries.js
+++ db/queries.js
@@ -36,9 +36,86 @@ async function getItemById(id) {
   return rows[0];
 }
 
+async function createCategory({ name, description }) {
+  const { rows } = await pool.query(
+    "INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *",
+    [name, description]
+  );
+
+  return rows[0];
+}
+
+async function updateCategory(id, { name, description }) {
+  const { rows } = await pool.query(
+    "UPDATE categories SET name = $1, description = $2 WHERE id = $3 RETURNING *",
+    [name, description, id]
+  );
+
+  return rows[0];
+}
+
+async function createItem({ name, category_id, age, sex, height_hands, color, price, description }) {
+  const query = `
+    INSERT INTO items (
+      name, category_id, age, sex, height_hands, color, price, description
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    RETURNING *
+  `;
+
+  const values = [
+    name,
+    category_id,
+    age,
+    sex,
+    height_hands,
+    color,
+    price,
+    description,
+  ];
+
+  const { rows } = await pool.query(query, values);
+  return rows[0];
+}
+
+async function updateItem(id, { name, category_id, age, sex, height_hands, color, price, description }) {
+  const query = `
+    UPDATE items
+    SET name = $1,
+        category_id = $2,
+        age = $3,
+        sex = $4,
+        height_hands = $5,
+        color = $6,
+        price = $7,
+        description = $8
+    WHERE id = $9
+    RETURNING *
+  `;
+
+  const values = [
+    name,
+    category_id,
+    age,
+    sex,
+    height_hands,
+    color,
+    price,
+    description,
+    id
+  ];
+
+  const { rows } = await pool.query(query, values);
+  return rows[0];
+}
+
 module.exports = {
   getAllCategories,
   getCategoryById,
   getItemsByCategoryId,
   getItemById,
+  createCategory,
+  updateCategory,
+  createItem,
+  updateItem,
 };
