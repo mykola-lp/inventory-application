87c96ebb690ed75ae6a8e5d2e9b6a1bb2f1e6b50
mykola-lp <mykola.sukhorab@gmail.com>
2026-09-01 09:02:38 +0300

    add items Router

---
+++ routes/itemsRouter.js
@@ -0,0 +1,16 @@
+// routes/itemsRouter.js
+
+const { Router } = require("express");
+
+const itemsController = require("../controllers/itemsController");
+
+const itemsRouter = Router();
+
+itemsRouter.get("/new", itemsController.itemCreateGet);
+itemsRouter.post("/new", itemsController.itemCreatePost);
+itemsRouter.get("/:id", itemsController.itemDetailGet);
+itemsRouter.get("/:id/update", itemsController.itemUpdateGet);
+itemsRouter.post("/:id/update", itemsController.itemUpdatePost);
+itemsRouter.post("/:id/delete", itemsController.itemDeletePost);
+
+module.exports = itemsRouter;
